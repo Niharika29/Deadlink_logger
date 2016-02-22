bd3f376e9d275fee2d18802dad207b03c4afddeb
Niharika Kohli <niharikakohli29@gmail.com>
2016-02-22 22:02:10 +0530

    Attempt more refactoring

--- public_html/api.php
+++ public_html/api.php
@@ -2,25 +2,35 @@
 
 require_once dirname(__FILE__) . '/../config.php';
 
-$wiki = isset($_GET['wiki']) ? $_GET['wiki'] : '';
-$pageId = isset($_GET['page']) ? $_GET['page'] : '';
-$revId = isset($_GET['rev']) ? $_GET['rev'] : '';
-$numLinks = isset($_GET['num']) ? $_GET['num'] : '';
-$botId = isset($_GET['id']) ? $_GET['id'] : '';
-$service = isset($_GET['service']) ? $_GET['service'] : '';
-$status = isset($_GET['status']) ? $_GET['status'] : '';
+function addLogRecord( $vars ) {
+	$wiki = isset( $vars['wiki']) ? $vars['wiki'] : '';
+	$pageId = isset($vars['page']) ? $vars['page'] : '';
+	$revId = isset($vars['rev']) ? $vars['rev'] : '';
+	$numLinks = isset($vars['num']) ? $vars['num'] : '';
+	$botId = isset($vars['id']) ? $vars['id'] : '';
+	$service = isset($vars['service']) ? $vars['service'] : '';
+	$status = isset($vars['status']) ? $vars['status'] : '';
 
-$link = mysqli_connect( $credentials['host'], $credentials['user'], $credentials['pass'], $credentials['db'] );
+	$link = mysqli_connect( $credentials['host'], $credentials['user'], $credentials['pass'], $credentials['db'] );
 
-$query = "INSERT INTO bot_log (wiki, page_id, rev_id, num_links, bot_id, service, status)
-          VALUES ( '$wiki' , '$pageId' , '$revId' , '$numLinks' , '$botId' , '$service' , '$status' )";
+	$query = "INSERT INTO bot_log (wiki, page_id, rev_id, num_links, bot_id, service, status)
+	          VALUES ( '$wiki' , '$pageId' , '$revId' , '$numLinks' , '$botId' , '$service' , '$status' )";
 
-$result = mysqli_query( $link, $query );
+	$result = mysqli_query( $link, $query );
 
-if ( $result ) {
-	return json_encode( 'true' );
-} else {
-	return json_encode( 'false' );
+	if ( $result ) {
+		return json_encode( 'true' );
+	} else {
+		return json_encode( 'false' );
+	}
+
+	echo $result;
 }
 
-echo $result;
+// $wiki = isset($_GET['wiki']) ? $_GET['wiki'] : '';
+// $pageId = isset($_GET['page']) ? $_GET['page'] : '';
+// $revId = isset($_GET['rev']) ? $_GET['rev'] : '';
+// $numLinks = isset($_GET['num']) ? $_GET['num'] : '';
+// $botId = isset($_GET['id']) ? $_GET['id'] : '';
+// $service = isset($_GET['service']) ? $_GET['service'] : '';
+// $status = isset($_GET['status']) ? $_GET['status'] : '';
--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,19 @@
 <?php
 
-$uri = $_SERVER['REQUEST_URI'];
+require_once( 'api.php' );
 
-echo $uri;
+if( isset( $_GET['id'] ) ) {
+	$vars = $_GET;
+	var_dump( $vars );
+	addLogRecord( $vars );
+	// $wiki = isset($_GET['wiki']) ? $_GET['wiki'] : '';
+	// $pageId = isset($_GET['page']) ? $_GET['page'] : '';
+	// $revId = isset($_GET['rev']) ? $_GET['rev'] : '';
+	// $numLinks = isset($_GET['num']) ? $_GET['num'] : '';
+	// $botId = isset($_GET['id']) ? $_GET['id'] : '';
+	// $service = isset($_GET['service']) ? $_GET['service'] : '';
+	// $status = isset($_GET['status']) ? $_GET['status'] : '';
+
+} else {
+	echo "Boo!";
+}
