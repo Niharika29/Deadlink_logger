85ee640a5e7e6f2891a98eed141b18fa0a883942
Niharika Kohli <niharikakohli29@gmail.com>
2016-02-25 20:14:23 +0530

    More testing

--- public_html/index.php
+++ public_html/index.php
@@ -19,31 +19,31 @@ $time = 'lweek';
 
 	$html = "<form name="f1" method="post">
 		<select name="time">
-			<option value="lweek" if( $_POST['time'] == 'lweek' ) echo"selected";>Last week</option>
-			<option value="lmonth" if( $_POST['time'] == 'lmonth' ) echo"selected";>Last month</option>
-			<option value="lyear" if( $_POST['time'] == 'lyear' ) echo"selected";>Last year</option>
+			<option value='lweek' if( ". $_POST['time'] ." == 'lweek' ) echo"selected";>Last week</option>
+			<option value='lmonth' if( ". $_POST['time'] ." == 'lmonth' ) echo"selected";>Last month</option>
+			<option value='lyear' if( ". $_POST['time'] ." == 'lyear' ) echo"selected";>Last year</option>
 		</select>
 
 		<select name="lang">
-			<option value="en" if( $_POST['lang'] == 'en' ) echo"selected";>en</option>
-			<option value="de" if( $_POST['lang'] == 'de' ) echo"selected";>de</option>
-			<option value="fr" if( $_POST['lang'] == 'fr' ) echo"selected";>fr</option>
-			<option value="hi" if( $_POST['lang'] == 'hi' ) echo"selected";>hi</option>
-			<option value="he" if( $_POST['lang'] == 'he' ) echo"selected";>he</option>
-			<option value="es" if( $_POST['lang'] == 'es' ) echo"selected";>es</option>
+			<option value="en" if( ". $_POST['lang'] ." == 'en' ) echo"selected";>en</option>
+			<option value="de" if( ". $_POST['lang'] ." == 'de' ) echo"selected";>de</option>
+			<option value="fr" if( ". $_POST['lang'] ." == 'fr' ) echo"selected";>fr</option>
+			<option value="hi" if( ". $_POST['lang'] ." == 'hi' ) echo"selected";>hi</option>
+			<option value="he" if( ". $_POST['lang'] ." == 'he' ) echo"selected";>he</option>
+			<option value="es" if( ". $_POST['lang'] ." == 'es' ) echo"selected";>es</option>
 		</select>
 
 		<select name="wiki">
-			<option value="wikipedia" if( $_POST['wiki'] == 'wikipedia' ) echo"selected";>wikipedia</option>
-			<option value="wikisource" if( $_POST['wiki'] == 'wikisource' ) echo"selected";>wikisource</option>
-			<option value="wikinews" if( $_POST['wiki'] == 'wikinews' ) echo"selected";>wikinews</option>
+			<option value="wikipedia" if( ". $_POST['wiki'] ." == 'wikipedia' ) echo"selected";>wikipedia</option>
+			<option value="wikisource" if( ". $_POST['wiki'] ." == 'wikisource' ) echo"selected";>wikisource</option>
+			<option value="wikinews" if( ". $_POST['wiki'] ." == 'wikinews' ) echo"selected";>wikinews</option>
 		</select>
 
 		<select name="bot">
-			<option value="all" if( $_POST['bot'] == 'all' ) echo"selected";>All bots</option>
-			<option value="1" if( $_POST['bot'] == '1' ) echo"selected";>Bot 1</option>
-			<option value="2" if( $_POST['bot'] == '2' ) echo"selected";>Bot 2</option>
-			<option value="3" if( $_POST['bot'] == '3' ) echo"selected";>Bot 3</option>
+			<option value="all" if( ". $_POST['bot'] ." == 'all' ) echo"selected";>All bots</option>
+			<option value="1" if( ". $_POST['bot'] ." == '1' ) echo"selected";>Bot 1</option>
+			<option value="2" if( ". $_POST['bot'] ." == '2' ) echo"selected";>Bot 2</option>
+			<option value="3" if( ". $_POST['bot'] ." == '3' ) echo"selected";>Bot 3</option>
 		</select>
 
 		<input type="submit" name="submit" value="Go" />
