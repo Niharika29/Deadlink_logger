b05596c9714e4eee9e20ff6b673b64c1d9dce102
Niharika Kohli <niharikakohli29@gmail.com>
2016-02-26 14:03:08 +0530

    Move form out

--- public_html/index.php
+++ public_html/index.php
@@ -12,12 +12,42 @@ function test_input($data) {
 	return $data;
 }
 
-// $bot = 'all';
-// $lang = 'en';
-// $wiki = 'wikipedia';
-// $time = 'lweek';
+$bot = 'all';
+$lang = 'en';
+$wiki = 'wikipedia';
+$time = 'lweek';
 
-$html = 'Hoops';
+	$html = "<form name='f1' method='post'>
+		<select name='time'>
+			<option value='lweek' if( ". $_POST['time'] ." == 'lweek' ) 'selected';>Last week</option>
+			<option value='lmonth' if( ". $_POST['time'] ." == 'lmonth' ) 'selected';>Last month</option>
+			<option value='lyear' if( ". $_POST['time'] ." == 'lyear' ) 'selected';>Last year</option>
+		</select>
+
+		<select name='lang'>
+			<option value='en' if( ". $_POST['lang'] ." == 'en' ) 'selected';>en</option>
+			<option value='de' if( ". $_POST['lang'] ." == 'de' ) 'selected';>de</option>
+			<option value='fr' if( ". $_POST['lang'] ." == 'fr' ) 'selected';>fr</option>
+			<option value='hi' if( ". $_POST['lang'] ." == 'hi' ) 'selected';>hi</option>
+			<option value='he' if( ". $_POST['lang'] ." == 'he' ) 'selected';>he</option>
+			<option value='es' if( ". $_POST['lang'] ." == 'es' ) 'selected';>es</option>
+		</select>
+
+		<select name='wiki'>
+			<option value='wikipedia if( ". $_POST['wiki'] ." == 'wikipedia' ) 'selected';>wikipedia</option>
+			<option value='wikisource' if( ". $_POST['wiki'] ." == 'wikisource' ) 'selected';>wikisource</option>
+			<option value='wikinews' if( ". $_POST['wiki'] ." == 'wikinews' ) 'selected';>wikinews</option>
+		</select>
+
+		<select name='bot'>
+			<option value='all' if( ". $_POST['bot'] ." == 'all' ) 'selected';>All bots</option>
+			<option value='1' if( ". $_POST['bot'] ." == '1' ) 'selected';>Bot 1</option>
+			<option value='2' if( ". $_POST['bot'] ." == '2' ) 'selected';>Bot 2</option>
+			<option value='3' if( ". $_POST['bot'] ." == '3' ) 'selected';>Bot 3</option>
+		</select>
+
+		<input type='submit' name='submit' value='Go' />
+	</form>";
 
 if ( isset( $_GET['id'] ) ) {
 	$vars = $_GET;
@@ -53,44 +83,10 @@ if ( isset( $_GET['id'] ) ) {
 						.'</tr>';
 			}
 			$html .= '</table>';
-			echo $html;
 		}
 	}
 }
 
-$form = "<form name='f1' method='post'>
-		<select name='time'>
-			<option value='lweek' if( ". $_POST['time'] ." == 'lweek' ) 'selected';>Last week</option>
-			<option value='lmonth' if( ". $_POST['time'] ." == 'lmonth' ) 'selected';>Last month</option>
-			<option value='lyear' if( ". $_POST['time'] ." == 'lyear' ) 'selected';>Last year</option>
-		</select>
-
-		<select name='lang'>
-			<option value='en' if( ". $_POST['lang'] ." == 'en' ) 'selected';>en</option>
-			<option value='de' if( ". $_POST['lang'] ." == 'de' ) 'selected';>de</option>
-			<option value='fr' if( ". $_POST['lang'] ." == 'fr' ) 'selected';>fr</option>
-			<option value='hi' if( ". $_POST['lang'] ." == 'hi' ) 'selected';>hi</option>
-			<option value='he' if( ". $_POST['lang'] ." == 'he' ) 'selected';>he</option>
-			<option value='es' if( ". $_POST['lang'] ." == 'es' ) 'selected';>es</option>
-		</select>
-
-		<select name='wiki'>
-			<option value='wikipedia if( ". $_POST['wiki'] ." == 'wikipedia' ) 'selected';>wikipedia</option>
-			<option value='wikisource' if( ". $_POST['wiki'] ." == 'wikisource' ) 'selected';>wikisource</option>
-			<option value='wikinews' if( ". $_POST['wiki'] ." == 'wikinews' ) 'selected';>wikinews</option>
-		</select>
-
-		<select name='bot'>
-			<option value='all' if( ". $_POST['bot'] ." == 'all' ) 'selected';>All bots</option>
-			<option value='1' if( ". $_POST['bot'] ." == '1' ) 'selected';>Bot 1</option>
-			<option value='2' if( ". $_POST['bot'] ." == '2' ) 'selected';>Bot 2</option>
-			<option value='3' if( ". $_POST['bot'] ." == '3' ) 'selected';>Bot 3</option>
-		</select>
-
-		<input type='submit' name='submit' value='Go' />
-	</form>";
-
-echo $form;
 echo $html;
 
 ?>
