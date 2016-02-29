d79c860714bdf9bc89ca0030f76faee5e2c7a902
Niharika Kohli <niharikakohli29@gmail.com>
2016-02-29 09:58:17 +0530

    Chart

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,7 +1,6 @@
 // Get context with jQuery - using jQuery's .get() method.
 var ctx = $( '#myChart' ).get( 0 ).getContext( '2d' );
 // This will get the first returned node in the jQuery collection.
-var myNewChart = new Chart( ctx );
 
 var data = {
 	labels: ["January", "February", "March", "April", "May", "June", "July"],
