7f66fcc9e2f182489ac20d72f8a2bf2ec38ed582
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-02 21:47:38 +0530

    Design legend

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -27,9 +27,7 @@ function displayChart( fixed, fvalues, nvalues ) {
 			}
 		]
 	};
-	// var options = {
-	// 	legendTemplate : "<ul><li><span style=\"background-color:#b47ec8; width:10px; height:10px\"></span>Links fixed</li><li><span style=\"background-color:#ef9048\"></span>Links fixed</li></ul>",
-	// }
+
 	var myLineChart = new Chart( ctx ).Line( data );
 	$( '#legend' ).html( myLineChart.generateLegend() );
 }
