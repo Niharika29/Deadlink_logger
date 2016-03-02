766490bc5f7047c3e60fa74963179d172a2a01cd
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-02 20:17:29 +0530

    Desc order by datetime

--- public_html/index.php
+++ public_html/index.php
@@ -30,15 +30,15 @@ if ( isset( $_GET['id'] ) ) {
 			$timeDiff = 'DATE_SUB(CURDATE(), INTERVAL 1 YEAR)';
 		}
 		if ( $bot == 'all' ) {
-			$query = "SELECT *, CAST( datetime AS DATE ) AS day FROM bot_log WHERE wiki = '".$url."' AND datetime >= $timeDiff";
+			$query = "SELECT *, CAST( datetime AS DATE ) AS day FROM bot_log WHERE wiki = '".$url."' AND datetime >= $timeDiff ORDER BY datetime DESC";
 			$chart = "SELECT datetime, CAST( datetime AS DATE ) AS day, SUM( links_fixed ) AS numf, SUM( links_not_fixed ) AS numn
 				FROM bot_log WHERE datetime >= $timeDiff AND wiki = '".$url."'
-				GROUP BY CAST( datetime AS DATE )";
+				GROUP BY CAST( datetime AS DATE ) ORDER BY datetime DESC";
 		} else {
-			$query = "SELECT *, CAST( datetime AS DATE ) AS day FROM bot_log WHERE wiki = '".$url."' AND datetime >= $timeDiff AND bot = '$bot'";
+			$query = "SELECT *, CAST( datetime AS DATE ) AS day FROM bot_log WHERE wiki = '".$url."' AND datetime >= $timeDiff AND bot = '$bot' ORDER BY datetime DESC";
 			$chart = "SELECT datetime, CAST( datetime AS DATE ) AS day, SUM( links_fixed ) AS numf, SUM( links_not_fixed ) AS numn
 				FROM bot_log WHERE datetime >= $timeDiff AND bot = '$bot' AND wiki = '".$url."'
-				GROUP BY CAST( datetime AS DATE )";
+				GROUP BY CAST( datetime AS DATE ) ORDER BY datetime DESC";
 		}
 		$chartData = mysqli_query( $link, $chart );
 		$dataf = array();
