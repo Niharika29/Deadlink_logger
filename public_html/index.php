8babf4e7e231ff42029f4f8eb0a17d3a8fb25b14
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-02 12:36:40 +0530

    Fix time range issues

--- public_html/index.php
+++ public_html/index.php
@@ -37,12 +37,12 @@ if ( isset( $_GET['id'] ) ) {
 			$timeDiff = 'DATE_SUB(CURDATE(), INTERVAL 1 YEAR)';
 		}
 		if ( $bot == 'all' ) {
-			$query = 'SELECT * FROM bot_log WHERE wiki = "' . $url . '" AND datetime >= $timeDiff';
+			$query = "SELECT * FROM bot_log WHERE wiki = "' . $url . '" AND datetime >= $timeDiff";
 			$chart = "SELECT datetime, CAST( datetime AS DATE ) AS day, SUM( num_links ) AS totalnum
 				FROM bot_log WHERE datetime >= $timeDiff
 				GROUP BY CAST( datetime AS DATE )";
 		} else {
-			$query = 'SELECT * FROM bot_log WHERE wiki = "' . $url . '" AND datetime >= $timeDiff AND bot_id = $bot';
+			$query = "SELECT * FROM bot_log WHERE wiki = "' . $url . '" AND datetime >= $timeDiff AND bot_id = $bot";
 			$chart = "SELECT datetime, CAST( datetime AS DATE ) AS day, SUM( num_links ) AS totalnum
 				FROM bot_log WHERE datetime >= $timeDiff AND bot_id = $bot
 				GROUP BY CAST( datetime AS DATE )";
@@ -53,7 +53,7 @@ if ( isset( $_GET['id'] ) ) {
 		while ( $row = $chartData->fetch_assoc() ) {
 			$data[$row['day']] = $row['totalnum'];
 		}
-		var_dump( $result );
+		var_dump( $query );
 		if ( $result->num_rows > 0 ) {
 			$html = '<table id="results">';
 			$html .= '<tr>
