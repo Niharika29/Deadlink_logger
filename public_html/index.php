f88c2a702ae4994435b52728e44eef232871b405
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-02 21:52:05 +0530

    Design legend

--- public_html/index.php
+++ public_html/index.php
@@ -130,7 +130,7 @@ if ( isset( $_GET['id'] ) ) {
 				<input type="submit" name="submit" id="submit" value="Go" />
 			</form>
 		</div>
-		<canvas id="bot-chart" style="width:900px;height:400px"></canvas>
+		<canvas id="bot-chart" style="width:900px;height:350px"></canvas>
 		<div id="legend"></div>
 		<?=$html?>
 		<link rel="stylesheet" type="text/css" href="css/index.css">
