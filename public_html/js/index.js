cb44c01182aaee94a0b5015452f21821550b7dc8
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-03 13:37:37 +0530

    Add stats

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -29,6 +29,6 @@ function displayChart( fixed, fvalues, nvalues, totalf, totalp ) {
 	};
 
 	var myLineChart = new Chart( ctx ).Line( data );
-	$( '#legend' ).html( myLineChart.generateLegend() + ' Pages fixed: ' + totalf + ' Pages processed: ' + totalp );
+	$( '#legend' ).html( myLineChart.generateLegend() + ' Pages fixed: ' + totalf + '  Pages processed: ' + totalp );
 }
 
