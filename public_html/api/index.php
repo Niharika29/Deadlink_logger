fdcf4745c80a771297e8c1f1443a342ca55dd58c
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-08 22:22:56 +0530

    Testing

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -5,7 +5,7 @@ require_once dirname(__FILE__) . '/../../config.php';
 // function addLogRecord() {
 	$link = mysqli_connect( $credentials['host'], $credentials['user'], $credentials['pass'], $credentials['db'] );
 	$vars = $_GET;
-	var_dump( $vars );
+	var_dump( $credentials );
 	foreach( $vars as $key => $value ) {
 		$vars[$key] = trim( mysqli_real_escape_string( $link, $value ) );
 	}
