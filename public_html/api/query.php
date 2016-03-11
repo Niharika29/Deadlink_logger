98f4834a2508a73d8fb0c52cef6159d0d0c16661
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-11 21:45:16 +0530

    Improve query output API

--- public_html/api/query.php
+++ public_html/api/query.php
@@ -3,33 +3,34 @@
 require_once dirname(__FILE__) . '/../../config.php';
 
 $vars = $_GET;
-// foreach( $vars as $key => $value ) {
-// 	$vars[$key] = trim( mysqli_real_escape_string( $value ) );
-// }
+$link = mysqli_connect( $credentials['host'], $credentials['user'], $credentials['pass'], $credentials['db'] );
 
-echo $vars;
+foreach( $vars as $key => $value ) {
+	$vars[$key] = trim( mysqli_real_escape_string( $value ) );
+}
+
+var_dump( $vars );
 
 // Case 1: wiki and page id given - get details about when last parsed and by which bot etc.
 if ( $vars['wiki'] !== null && $vars['id'] !== null ) {
 	$wiki = $vars['wiki'];
 	$page_id = $vars['id'];
 	$query = "SELECT * FROM bot_log WHERE wiki = '$wiki' AND page_id = $page_id LIMIT 1";
-	generateResult( $query, $credentials );
+	generateResult( $query, $link );
 // Case 2: wiki and bot given - check which page last parsed by bot on that wiki
 } elseif ( $vars['wiki'] !== null && $vars['bot'] !== null ) {
 	$wiki = $vars['wiki'];
 	$bot = $vars['bot'];
 	$query = "SELECT * FROM bot_log WHERE wiki = '$wiki' AND bot = '$bot' LIMIT 1";
-	generateResult( $query, $credentials );
+	generateResult( $query, $link );
 // Case 3: bot given - check which page last edited on which wiki and when by the bot
 } elseif ( $vars['bot'] !== null ) {
 	$bot = $vars['bot'];
 	$query = "SELECT * FROM bot_log WHERE bot = '$bot' LIMIT 1";
-	generateResult( $query, $credentials );
+	generateResult( $query, $link );
 }
 
-function generateResult( $query, $credentials ) {
-	$link = mysqli_connect( $credentials['host'], $credentials['user'], $credentials['pass'], $credentials['db'] );
+function generateResult( $query, $link ) {
 	$result = mysqli_query( $link, $query );
 	var_dump( $query );
 	if ( $result->num_rows > 0 ) {
