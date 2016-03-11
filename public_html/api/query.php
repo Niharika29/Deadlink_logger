3d4c5291d9cba49f8b179f1a83da92fdce49d9c0
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-11 21:41:01 +0530

    Improve query output API

--- public_html/api/query.php
+++ public_html/api/query.php
@@ -29,7 +29,7 @@ if ( $vars['wiki'] !== null && $vars['id'] !== null ) {
 function generateResult( $query, $credentials ) {
 	$link = mysqli_connect( $credentials['host'], $credentials['user'], $credentials['pass'], $credentials['db'] );
 	$result = mysqli_query( $link, $query );
-	var_dump( $result );
+	var_dump( $query );
 	if ( $result->num_rows > 0 ) {
 		$row = $result->fetch_assoc();
 		echo json_encode( $row );
