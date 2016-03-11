9a3ad6e2894d8d1b16aa267d169df1b5cf37789c
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-11 19:17:37 +0530

    Example query output

--- public_html/api/query.php
+++ public_html/api/query.php
@@ -1,3 +1,32 @@
 <?php
 
-echo 'lala';
+require_once dirname(__FILE__) . '/../../config.php';
+
+$link = mysqli_connect( $credentials['host'], $credentials['user'], $credentials['pass'], $credentials['db'] );
+$vars = $_GET;
+foreach( $vars as $key => $value ) {
+	$vars[$key] = trim( mysqli_real_escape_string( $link, $value ) );
+}
+
+$query = $vars['query'];
+
+if ( $query == 'true' && isset( $vars['wiki'] && isset( $vars['id'] ) ) ) {
+	$wiki = $vars['wiki'];
+	$page_id = $vars['id'];
+	echo $wiki, $page_id;
+	$query = "SELECT * FROM bot_log WHERE wiki = $wiki AND page_id = $page_id LIMIT 1";
+	$result = mysqli_query( $link, $query );
+	if ( $result ) {
+		$data = array();
+		$row = $result->fetch_assoc();
+		var_dump( $row );
+		foreach ( $row as $key => $value ) {
+			$data[$key] = $value;
+		}
+		var_dump( $data );
+	} else {
+		echo json_encode( 'false' );
+	}
+} else {
+	echo json_encode( 'false' );
+}
