7c7cdede36da863e80ccc26a6c03e7cd2e7fe432
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-11 21:41:45 +0530

    Improve query output API

--- public_html/api/query.php
+++ public_html/api/query.php
@@ -7,6 +7,7 @@ foreach( $vars as $key => $value ) {
 	$vars[$key] = trim( mysqli_real_escape_string( $value ) );
 }
 
+echo $vars;
 // Case 1: wiki and page id given - get details about when last parsed and by which bot etc.
 if ( $vars['wiki'] !== null && $vars['id'] !== null ) {
 	$wiki = $vars['wiki'];
