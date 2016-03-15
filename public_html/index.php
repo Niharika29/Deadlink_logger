0ad328872449f80f1188e6d72d458a63163cfaa0
Niharika Kohli <niharikakohli29@gmail.com>
2016-03-15 19:52:02 +0530

    Switch to chart.js fork for label spacing support

--- public_html/index.php
+++ public_html/index.php
@@ -87,7 +87,7 @@ if ( isset( $_POST['submit'] ) ) {
 <html>
 	<head>
 		<title>IA bot logs</title>
-<script src="../vendor/Chart.min.js"></script>
+<script src="js/Chart.min.js" type="text/javascript"></script>
 <script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="js/index.js" type="text/javascript"></script>
 <script type="text/javascript">
