dac468fdda47c49019aee144ad99917eab05ba8b
Niharika Kohli <niharikakohli29@gmail.com>
2017-03-07 10:23:20 -0800

    Make default query just pull top 100 records

--- public_html/index.php
+++ public_html/index.php
@@ -16,8 +16,7 @@ $totalf = 0;
 $botNames = array();
 
 // Default query
-$query = "SELECT *, CAST( datetime AS DATE ) AS day FROM bot_log WHERE wiki = 'en.wikipedia.org'
-			AND datetime >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) ORDER BY datetime DESC LIMIT 100";
+$query = "SELECT *, CAST( datetime AS DATE ) AS day FROM bot_log ORDER BY datetime DESC LIMIT 100";
 $chart = "SELECT datetime, CAST( datetime AS DATE ) AS day, SUM( links_fixed ) AS numf, SUM( links_not_fixed ) AS numn
 			FROM bot_log WHERE datetime >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) AND wiki = 'en.wikipedia.org'
 			GROUP BY CAST( datetime AS DATE ) ORDER BY datetime ASC";
